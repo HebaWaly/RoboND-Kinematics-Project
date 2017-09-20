da34cedf16b75e0579f9df4b342874932e874d1f
Heba <heba.waly@gmail.com>
2017-09-20 00:45:17 -0700

    IK_server

--- IK_debug.py
+++ IK_debug.py
@@ -64,12 +64,132 @@ def test_code(test_case):
 
     ## Insert IK code here!
     
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
+    ### Your FK code here
+    # Create symbols
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')   
+    # Create Modified DH parameters
+    s = {alpha0:      0, a0:      0, d1:  0.75, q1:          q1,
+         alpha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2. + q2,
+         alpha2:      0, a2:   1.25, d3:     0, q3:          q3,
+         alpha3: -pi/2., a3: -0.054, d4:   1.5, q4:          q4,
+         alpha4:  pi/2., a4:      0, d5:     0, q5:          q5,
+         alpha5: -pi/2., a5:      0, d6:     0, q6:          q6,
+         alpha6:      0, a6:      0, d7: 0.303, q7:           0}            
+    # Define Modified DH Transformation matrix
+    def transform(alpha, a, d, q):
+        T = Matrix([[           cos(q),           -sin(q),          0,              a],
+              [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+              [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+              [                0,                 0,           0,             1]])
+        return T
+    # Create individual transformation matrices
+    T0_1 = transform(alpha0, a0, d1, q1)
+    T1_2 = transform(alpha1, a1, d2, q2)
+    T2_3 = transform(alpha2, a2, d3, q3)
+    T3_4 = transform(alpha3, a3, d4, q4)
+    T4_5 = transform(alpha4, a4, d5, q5)
+    T5_6 = transform(alpha5, a5, d6, q6)
+    T6_G = transform(alpha6, a6, d7, q7)
+
+    T0_1 = T0_1.subs(s)
+    T1_2 = T1_2.subs(s)
+    T2_3 = T2_3.subs(s)
+    T3_4 = T3_4.subs(s)
+    T4_5 = T4_5.subs(s)
+    T5_6 = T5_6.subs(s)
+    T6_G = T6_G.subs(s)
+
+    T0_2 = T0_1 * T1_2
+    T0_3 = T0_2 * T2_3
+    T0_4 = T0_3 * T3_4
+    T0_5 = T0_4 * T4_5
+    T0_6 = T0_5 * T5_6
+    T0_G = T0_6 * T6_G
+    # angles = {q1: 0., q2: -pi/2., q3: 0., q4: 0., q5: 0., q6: 0., q7: 0.}
+                  
+    # Correction rotation
+    R_z = Matrix([[cos(pi), -sin(pi), 0, 0],
+            [sin(pi), cos(pi), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]])
+    R_y = Matrix([[cos(-pi/2.), 0, sin(-pi/2.), 0],
+            [0, 1, 0, 0],
+            [-sin(-pi/2.), 0, cos(-pi/2.), 0],
+            [0, 0, 0, 1]])
+    R_corr = R_z * R_y
+
+    T0_G = T0_G * R_corr
+    # Extract rotation matrices from the transformation matrices
+    #
+    #
+    ###
+
+    # Initialize service response
+
+    # Extract end-effector position and orientation from request
+    # px,py,pz = end-effector position
+    # roll, pitch, yaw = end-effector orientation
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+        [req.poses[x].orientation.x, req.poses[x].orientation.y,
+            req.poses[x].orientation.z, req.poses[x].orientation.w])
+    # Rotation Angles:
+    R_x = Matrix([[ 1,              0,        0],
+                  [ 0,        cos(q1), -sin(q1)],
+                  [ 0,        sin(q1),  cos(q1)]])
+
+    R_y = Matrix([[ cos(q2),        0,  sin(q2)],
+                  [       0,        1,        0],
+                  [-sin(q2),        0,  cos(q2)]])
+
+    R_z = Matrix([[ cos(q3), -sin(q3),        0],
+                  [ sin(q3),  cos(q3),        0],
+                  [ 0,              0,        1]])
+    Rot_G = R_z.subs(q3, yaw) * R_y.subs(q2, pitch) * R_x.subs(q1, roll)
+    
+    ### Your IK code here
+    # Compensate for rotation discrepancy between DH parameters and Gazebo
+    
+    R_correction = R_z.subs(q3, pi) * R_y.subs(q2, -pi/2.)
+    Rot_G = Rot_G * R_correction
+
+    # Calculate joint angles using Geometric IK method
+    EE = Matrix([[px],
+                 [py],
+                 [pz]])
+    WC = EE - 0.303 * Rot_G[:,2]
+    
+    theta1 = atan2(WC[1],WC[0])
+    
+    s1 = sqrt(pow(WC[0],2) + pow(WC[1],2)) - 0.35 #WC[0] - 0.35
+    s2 = WC[2] - 0.75
+    s3 = sqrt(pow(s1, 2) + pow(s2, 2))
+    s4 = sqrt(pow(-0.054, 2) + pow(1.5, 2))
+    s5 = 1.25
+    
+    beta1 = atan2(s2, s1)
+    beta2 = acos((pow(s3, 2) + pow(s5, 2) - pow(s4, 2))/(2*s3*s5))
+    theta2 = pi/2. - beta1 - beta2
+  
+    beta3 = acos((pow(s4, 2) + pow(s5, 2) - pow(s3, 2))/(2*s4*s5))
+    beta4 = atan2(-0.054,1.5)
+    theta3 = pi/2. - beta3 - beta4
+    
+    R0_3 = T0_3[0:3,0:3].evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+    R3_6 = R0_3.inv("LU") * Rot_G
+    
+    # Also R3_6 can be calculated from: T3_6 = T3_4*T4_5*T5_6
+    # solving both sides:
+    theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+    theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+    theta5 = atan2(sqrt(pow(R3_6[0,2],2)+pow(R3_6[2,2],2)),R3_6[1,2])
+    ###
 
     ## 
     ########################################################################################
@@ -79,7 +199,7 @@ def test_code(test_case):
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
-
+    FK = T0_G.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
     ## End your code input for forward kinematics here!
     ########################################################################################
 
